e3ffe045176849d147fe2393409e73252bc89367
Alex <alexe14ernyakov@mail.ru>
2024-07-09 13:01:18 +0300

    Working Lab

---
+++ task2.js
@@ -0,0 +1,8 @@
+let body = document.body
+
+for(let i = 0; i < 100; i++){
+    let div = document.createElement("div")
+    div.className = "n" + (i + 1)
+    div.textContent = String(i + 1)
+    body.appendChild(div)
+}
